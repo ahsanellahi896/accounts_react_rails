e72573471a482e94d66d5be1eef45b0c691862aa
Ahsan Ellahi <ahsan.ellahi@square63.com>
2016-07-21 18:21:39 +0500

    Record: Implemented basic flow with react

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160721072211) do
+
+  create_table "records", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.date     "date"
+    t.float    "amount",     limit: 24
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
